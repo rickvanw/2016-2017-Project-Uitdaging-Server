a0e69d609c1e3ae5b59802c3d61728951e782ba0
maurice_2 <mauricebergboer@hotmail.com>
2017-05-24 23:26:58 +0200

    Aangepaste/nieuwe queries i.v.m. verandering db en nieuwe toevoegingen voor calls

--- resources/treatment.js
+++ resources/treatment.js
@@ -16,14 +16,17 @@ var config = require('./config.js');
 var utils = require('./utils.js');
 
 router.post('/add', function (req, res) {
+    // TODO Als er al een behandelplan bestaat, mag er niet een nieuwe worden gegenereerd!
     console.log("JOEgegergrRR");
-    var start_date = req.body.start_date;
-    var end_date = req.body.end_date;
+    var user_id = req.decoded.user_id;
+    var start_date = utils.getCurrentDate();
+    var end_date = utils.getEndDate();
 
+    console.log("user id: " + user_id);
     console.log("start date: " + start_date);
     console.log("end date: " + end_date);
 
-    var query = 'INSERT INTO treatment (start_date, end_date) VALUES ("' + start_date + '", "' + end_date + '");';
+    var query = 'INSERT INTO treatment (user_id, start_date, end_date) VALUES ("' + user_id + '", "' + start_date + '", "' + end_date + '");';
 
     connection.query(query, function (err) {
         if (err) {
@@ -42,27 +45,29 @@ router.get('/exercises-day', function (req, res) {
     var user_id = req.decoded.user_id;
     var date = req.header('day');
     console.log(JSON.stringify(req.headers));
+    console.log("user id: " + user_id);
     console.log("Date: " + date);
 
     var query = 'SELECT e.*, te.rating_user, te.done FROM exercise AS e '+
-    'INNER JOIN treatment_exercise AS te ON e.exercise_id = te.exercise_id '+
-    'INNER JOIN user_treatment AS ut ON te.treatment_id = ut.treatment_id '+
-    'INNER JOIN treatment AS t ON ut.treatment_id = t.treatment_id '+
-    'WHERE ut.user_id = '+ user_id + ' '+
-    'AND t.end_date >= "' + utils.getCurrentDate() +'" '+
-    'AND t.start_date <= "' + utils.getCurrentDate() +'" '+
-    'AND todo_datetime = "' + date +'" ';
+    'INNER JOIN treatment_exercise AS te ON te.exercise_id = e.exercise_id '+
+    'INNER JOIN treatment AS t ON t.treatment_id = te.treatment_id '+
+    'WHERE t.user_id = ' + user_id + ' '+
+    'AND t.end_date >= "' + utils.getCurrentDate() + '" '+
+    'AND t.start_date <= "' + utils.getCurrentDate() + '" ' +
+        // +'" '+
+    'AND te.todo_datetime = "' + date +'" ';
 
     connection.query(query, function (err, result) {
         if (err){
             res.status(404).send("Niet gevonden");
             return;
         }
+        console.log("result: " + result);
         res.status(200).json(result);
     });
 });
 
-router.get('/generate-day-exercises', function (req, res) {
+router.get('/generate-exercises', function(req, res) {
     var user_id = req.decoded.user_id;
     var current_date = utils.getCurrentDate();
 
@@ -77,11 +82,11 @@ router.get('/generate-day-exercises', function (req, res) {
 
         for(i = 0; i < result.length; i++){
             var exercise_id = result[i].exercise_id;
+            console.log("exercise_id: " + exercise_id);
             var query = 'INSERT INTO treatment_exercise (treatment_id, exercise_id, todo_datetime) ' +
-                    'VALUES ((SELECT ut.treatment_id FROM user_treatment AS ut ' +
-                    'INNER JOIN treatment AS t ON t.treatment_id = ut.treatment_id ' +
-                    'WHERE "' + current_date + '" between t.start_date AND t.end_date ' +
-                    'AND ut.user_id = ' + user_id + '), ' + exercise_id + ', "' + utils.getCurrentDateTime() + '")';
+                    'VALUES ((SELECT te.treatment_id FROM treatment AS te ' +
+                    'WHERE "' + current_date + '" between te.start_date AND te.end_date ' +
+                    'AND te.user_id = ' + user_id + '), ' + exercise_id + ', "' + utils.getCurrentDateTime() + '")';
 
             connection.query(query, function (err, result) {
                 if (err) {
@@ -100,26 +105,28 @@ router.put('/exercise-done', function (req, res) {
     // TODO token
 
     var exerciseId = req.body.exerciseId;
-    var done = req.body.done;
+    var done = req.body.isDone;
     var user_id = req.decoded.user_id;
+
+    console.log("exerciseId: " + exerciseId);
+    console.log("done: " + done);
+    console.log("user_id: " + user_id);
 
     // TODO token check
     var query;
     if (done == 1) {
         query = 'UPDATE treatment_exercise AS te '+
-            'INNER JOIN user_treatment AS ut ON te.treatment_id = ut.treatment_id '+
-            'INNER JOIN treatment AS t ON ut.treatment_id = t.treatment_id '+
+            'INNER JOIN treatment AS t ON t.treatment_id = te.treatment_id '+
             'SET te.done = 1 '+
-            'WHERE ut.user_id = '+ user_id +' '+
+            'WHERE t.user_id = '+ user_id +' '+
             'AND te.exercise_id = '+ exerciseId + ' '+
             'AND t.end_date >= "' + utils.getCurrentDate() +'" '+
             'AND t.start_date <= "' + utils.getCurrentDate() +'" ';
     }else{
         query = 'UPDATE treatment_exercise AS te '+
-            'INNER JOIN user_treatment AS ut ON te.treatment_id = ut.treatment_id '+
-            'INNER JOIN treatment AS t ON ut.treatment_id = t.treatment_id '+
+            'INNER JOIN treatment AS t ON t.treatment_id = te.treatment_id '+
             'SET te.done = -1 '+
-            'WHERE ut.user_id = '+ user_id +' '+
+            'WHERE t.user_id = '+ user_id +' '+
             'AND te.exercise_id = '+ exerciseId + ' '+
             'AND t.end_date >= "' + utils.getCurrentDate() +'" '+
             'AND t.start_date <= "' + utils.getCurrentDate() +'" ';
@@ -133,6 +140,7 @@ router.put('/exercise-done', function (req, res) {
             return;
         }
 
+        console.log("succes: " + done);
         res.status(200).send(done);
     });
 });
