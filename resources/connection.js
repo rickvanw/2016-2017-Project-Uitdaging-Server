75b5b85eb109d62bf0f0c513942050dbe9dcdebc
rickvanw <rickvanweersel@hotmail.com>
2017-06-08 13:06:10 +0200

    Changed to production database

--- resources/connection.js
+++ resources/connection.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
     host: '178.21.112.250',
     user: 'uitdaging',
     password: 'ehi2va15',
-    database: 'customer'
+    database: 'production'
 });
 
 connection.connect();
