430dbe7489ac8d534a94e1e029985e1cbb16f0f2
Desi <d.esi@hotmail.com>
2017-06-21 11:21:27 +0200

    getEvaluations changed

--- resources/complaint.js
+++ resources/complaint.js
@@ -9,6 +9,8 @@ var router = express.Router();
 
 module.exports = router;
 
+var fs = require('fs');
+
 var connection = require('./connection.js');
 var config = require('./config.js');
 var utils = require('./utils.js');
@@ -16,28 +18,36 @@ var utils = require('./utils.js');
 /**
  * GET all complaints in array
  */
+// router.get('', function (req, res) {
+//
+//     var query = "SELECT * FROM complaint";
+//     connection.query(query, function (err, result) {
+//         if (err){
+//             res.status(400).json("ERROR");
+//             return;
+//         }
+//         res.status(200).json(result);
+//     });
+// });
+
 router.get('', function (req, res) {
+    console.log("checkkk");
+    var user_id = req.decoded.user_id;
+    console.log("check " + user_id);
 
-    var query = "SELECT * FROM complaint";
-    connection.query(query, function (err, result) {
-        if (err){
-            res.status(400).json("ERROR");
-            return;
-        }
-        res.status(200).json(result);
-    });
-});
-
-router.get('complaint-names', function (req, res) {
-    var query = "SELECT c.name, tc.treatment_id FROM complaint " +
-        "INNER JOIN treatment_complaint ON tc.complaint_id = c.complaint_id" +
-        "INNER JOIN treatment ON treatment.treatment_id = tc.treatment_id";
+    var query = "SELECT c.name, tc.treatment_id FROM complaint AS c  " +
+        "INNER JOIN treatment_complaint AS tc ON tc.complaint_id = c.complaint_id " +
+        "INNER JOIN treatment AS t ON t.treatment_id = tc.treatment_id " +
+        "WHERE t.user_id = " + user_id;
 
     connection.query(query, function (err, result) {
         if (err){
             res.status(400).json("ERROR");
             return;
         }
+
+        console.log("&&&&&&&succes");
+        console.log(result);
         res.status(200).json(result);
     });
 });
