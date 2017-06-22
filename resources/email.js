52d253e4b786ce44bd65c617b63b307ba9b00ef2
rubenassink <ruben_assink@hotmail.com>
2017-06-22 12:51:58 +0200

    US320: email.js // Transporter object + sendmail function

---
+++ resources/email.js
@@ -0,0 +1,36 @@
+var nodemailer = require('nodemailer');
+
+var transporter;
+
+var email = function () {
+    // Transporter object using the gmail service
+    transporter = nodemailer.createTransport({
+        service: 'gmail',
+        secure: false,
+        port: 25,
+        auth: {
+            user: 'komnuinbeweging@gmail.com',
+            pass: 'kominbeweging'
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+};
+
+email.prototype.sendEmail = function (mailOptions) {
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);
+            return false;
+        }
+        return true;
+    });
+};
+
+module.exports = new email();
+
+
+
+
+
